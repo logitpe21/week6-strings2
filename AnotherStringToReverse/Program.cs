0bc143b0d93b0ff2bb2fca3adf026277898e6392
Julia Aasmaa <my_e_10@hotmail.com>
2021-10-06 10:50:25 +0300

    week 6 more strings

---
+++ AnotherStringToReverse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AnotherStringToReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab lauset "Hello, World!" tagurpidi
+            string helloW = "Hello, world!";
+
+            for(int i = helloW.Length - 1; i >= 0; i--)
+            {
+                Console.Write(helloW[i]);
+            }
+        }
+    }
+}
